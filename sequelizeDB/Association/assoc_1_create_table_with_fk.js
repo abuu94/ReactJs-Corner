c1aa4ca9ed0da42a0c6bb49e43412a50ea04537e
Abubakar Omar <45381191+abuu94@users.noreply.github.com>
2025-02-13 08:22:47 +0300

    FK table, using 1 to 1 relationship
    Create Country and Capital table with association of 1 to 1 relationship. 
    Forgeting freezetable:true make tables pluralised in mysql.
    Forgeting foreignKey:customeName make default  id.

---
+++ sequelizeDB/Association/assoc_1_create_table_with_fk.js
@@ -0,0 +1,50 @@
+const { raw } = require('mysql2');
+const Sequelize = require('sequelize');
+const {DataTypes,Op}=Sequelize;
+//npm install bcrypt
+const bcrypt = require('bcrypt');
+//npm install zlib
+const zlib = require('zlib');
+const { type } = require('os');
+const { timeStamp, error } = require('console');
+const sequelize = new Sequelize('sequelizedb', 'root', 'toor',{
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql', 
+});
+sequelize.authenticate().then(()=>{
+    console.log('Done: Connection has been established successfully.'); 
+}).catch((error)=>{
+    console.error('Fail: Unable to connect to the database:', error);
+});
+
+const Country = sequelize.define('country',{
+    countryName:{
+        type:DataTypes.STRING,
+        unique:true
+    }  
+  },{
+    // freezeTableName:true,
+    timestamp:false
+  });
+
+const Capital=sequelize.define('capital',{
+    capitalName:{
+        type:DataTypes.STRING,
+        unique:true
+    }
+
+    },{
+    // freezeTableName:true,
+    timestamp:false
+});
+
+Country.HasOne(Capital);  
+//Relationship : (Capital,{foreignKey:'countryID'})
+// You can provide custome FK name
+sequelize.sync({alter:true}).then(()=>{
+
+}).catch((error)=>{
+    console.log(error);
+});
+
