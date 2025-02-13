c10640068578af2701cc61cf5e711dd2c12f3126
Abubakar Omar <45381191+abuu94@users.noreply.github.com>
2025-02-13 12:05:29 +0300

    Remove FK of one record
    It will not delete a record but it will set Fk to null.
    That means that post is no longer belong to the deleted FK.

---
+++ sequelizeDB/Association/many_4_remove_fk_of _single_child_record.js
@@ -0,0 +1,59 @@
+const { raw } = require('mysql2');
+const Sequelize = require('sequelize');
+const {DataTypes,Op}=Sequelize;
+const bcrypt = require('bcrypt');
+const zlib = require('zlib');
+const { type } = require('os');
+const { timeStamp, error } = require('console');
+const sequelize = new Sequelize('sequelizedb', 'root', 'toor',{
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql', 
+});
+sequelize.authenticate().then(()=>{
+    console.log('Done: Connection has been established successfully.'); 
+}).catch((error)=>{
+    console.error('Fail: Unable to connect to the database:', error);
+});
+
+const User = sequelize.define('user',{
+    username:{
+        type:DataTypes.STRING
+    },
+    password:{
+        type:DataTypes.STRING
+    }
+},{
+    timestamps:false
+});
+
+const Post = sequelize.define('post',{
+    message:{
+        type:DataTypes.STRING
+    }
+
+},{
+    timestamps:false
+});
+
+User.hasMany(Post);
+Post.belongsTo(User);
+
+let user,posts;
+sequelize.sync({alter:true}).then(()=>{
+    //addPosts
+    return User.findOne({ where:{username:'abuu'}});
+}).then((data)=>{
+    user=data;
+    return Post.findOne();
+}).then((data)=>{
+    posts=data;
+    return user.removePost(posts);
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch(()=>{
+    console.log(error);
+    
+});
