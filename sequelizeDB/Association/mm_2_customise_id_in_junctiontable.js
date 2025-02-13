d1787325fe1d84afe4d594aaa409fc51ebd83c1c
Abubakar Omar <45381191+abuu94@users.noreply.github.com>
2025-02-13 13:53:13 +0300

    use Customise ID in junction table.
    use Customise ID in junction table during relationship definition.

---
+++ sequelizeDB/Association/mm_2_customise_id_in_junctiontable.js
@@ -0,0 +1,56 @@
+const { raw } = require('mysql2');
+const Sequelize = require('sequelize');
+const {DataTypes,Op}=Sequelize;
+const bcrypt = require('bcrypt');
+const zlib = require('zlib');
+const { type } = require('os');
+const { timeStamp, error, time } = require('console');
+const sequelize = new Sequelize('sequelizedb', 'root', 'toor',{
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql', 
+});
+sequelize.authenticate().then(()=>{
+    console.log('Done: Connection has been established successfully.'); 
+}).catch((error)=>{
+    console.error('Fail: Unable to connect to the database:', error);
+});
+
+const Customer = sequelize.define('customer',{
+    customerName:{
+        type:DataTypes.STRING
+    }
+},{
+    timestamps:false
+});
+
+const Product = sequelize.define('product',{
+    productName:{
+        type:DataTypes.STRING
+
+    }
+},{
+    timestamps:false
+});
+
+// const CustomerProduct = sequelize.define('customerproduct',{
+
+// });
+
+// customerproduct is ajunction table
+Customer.belongsToMany(Product,{
+    through:'customerproduct',
+    foreignKey:'customer_id'
+});
+Product.belongsToMany(Customer,{
+    through:'customerproduct',
+    foreignKey:'product_id'
+
+    });
+
+sequelize.sync({alter:true}).then(()=>{
+    console.log('Tables created successfully');
+}).then((err)=>{
+    console.log(err);
+    
+});
